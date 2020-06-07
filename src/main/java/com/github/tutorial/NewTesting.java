027584fc57483915c594a324fa09c2170ee39406
sunny <sunny.keila24@gmail.com>
2020-06-07 23:34:58 +0200

    A BBBB

---
+++ src/main/java/com/github/tutorial/NewTesting.java
@@ -0,0 +1,14 @@
+package com.github.tutorial;
+
+public class NewTesting {
+
+	public static void main(String[] args) {
+private int id;
+
+// ddd
+
+		
+		System.out.println("-------------");
+	}
+
+}
